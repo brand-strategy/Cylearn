d2be6653377a6d27734c934ffbe6359499319cc3
gbwlxhd97@hri.co.kr <gbwlxhd97@hri.co.kr>
2024-08-26 15:45:47 +0900

    yml ㄷ도    도    되    됟되돌리기

--- generate_log.py
+++ generate_log.py
@@ -1,35 +1,25 @@
 import os
-from bs4 import BeautifulSoup
 
 # HTML 파일을 검색할 디렉토리
 directory = '.'
 
 # 추가할 <div> 태그
-div_content = '<div>Your content here</div>'
+div_tag = '<div>Your content here</div>'
 
 # 디렉토리 내의 모든 HTML 파일을 처리
 for filename in os.listdir(directory):
     if filename.endswith('.html'):
         filepath = os.path.join(directory, filename)
-        
         # 파일 읽기
-        with open(filepath, 'r', encoding='utf-8') as file:
+        with open(filepath, 'r') as file:
             content = file.read()
         
-        # BeautifulSoup을 사용하여 HTML 파싱 및 수정
-        soup = BeautifulSoup(content, 'html.parser')
+        # <div> 태그 추가
+        if div_tag not in content:
+            content += f'\n{div_tag}\n'
         
-        # <body> 태그 찾기
-        body_tag = soup.find('body')
-        if body_tag:
-            # <body> 태그의 끝에 <div> 추가
-            if not body_tag.find('div', text='Your content here'):
-                new_div = soup.new_tag('div')
-                new_div.string = 'Your content here'
-                body_tag.append(new_div)
-            
-            # 수정된 HTML 파일을 다시 저장
-            with open(filepath, 'w', encoding='utf-8') as file:
-                file.write(str(soup))
+            # 파일 쓰기
+            with open(filepath, 'w') as file:
+                file.write(content)
         
         print(f'Updated {filename}')
